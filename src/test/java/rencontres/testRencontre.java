07f10960f7e64f666374170ffa2c8ed3e503ed63
Chaib <59232013+ChaibOuss@users.noreply.github.com>
2021-12-16 15:13:38 +0100

    add test unitaires pour les joueurs

--- src/test/java/rencontres/testRencontre.java
+++ src/test/java/rencontres/testRencontre.java
@@ -1,6 +1,8 @@
 package rencontres;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,11 +15,22 @@ public class testRencontre {
 		 Club club2=new Club("club2") ;
 		 int sc1=1;
 		 int sc2=2;
-		Rencontre r=new Rencontre(club1,club2,sc1,sc2);// Créer une rencontre
-		assertEquals(1,r.getscoreClub1());
-		assertEquals(2,r.getscoreClub2());
-		assertEquals(club1,r.getClub1());
-		assertEquals(club2,r.getClub2());
+		 int mj1 = club1.get_mj();
+		 int mj2 = club2.get_mj();
+		 int point2 = club2.get_nbpoints();
+		 Rencontre r =new Rencontre(club1,club2,sc1,sc2);// CrÃ©er une rencontre
+		 assertEquals(1,r.getscoreClub1());
+		 assertEquals(2,r.getscoreClub2());
+		 assertEquals(club1,r.getClub1());
+		 assertEquals(club2,r.getClub2());
+		 assertEquals(mj1+1,r.getClub1().get_mj());
+		 assertEquals(mj2+1,r.getClub2().get_mj());
+		 assertEquals(point2+3,r.getClub2().get_nbpoints());
+		 for (int i=0; i<19;i++) {
+	        	Joueur joueur = club2.getListJoueur().get(i);
+	        	assertEquals(1000,joueur.getNombrePoint());
+	        }
+
 		
 	}
 	/************** tester les getter ***************************/
@@ -29,7 +42,7 @@ public class testRencontre {
 		 Club club2=new Club("club2") ;
 		 int sc1=1;
 		 int sc2=2;
-		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// Créer une rencontre
+		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// CrÃ©er une rencontre
 		    assertEquals(1,r.getscoreClub1());
 			assertEquals(2,r.getscoreClub2());
 			assertEquals(club1,r.getClub1());
@@ -43,19 +56,19 @@ public class testRencontre {
 		 Club club2=new Club("club2") ;
 		 int sc1=1;
 		 int sc2=2;
-		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// Créer une rencontre
-		 r.Valider();// la rencontre sera validé tant que les 2 scores sont positif 
+		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// CrÃ©er une rencontre
+		 r.Valider();// la rencontre sera validÃ© tant que les 2 scores sont positif 
 		Assert.assertTrue(r.getIsValide());
 		 }
 	@Test
 	public final void testErrorMessage() {
 		 Club club1=new Club("club1");
 		 Club club2=new Club("club2") ;
-		 int sc1=-1; // score négatif pour tester si le message d'erreur sera retourné 
+		 int sc1=-1; // score nÃ©gatif pour tester si le message d'erreur sera retournÃ© 
 		 int sc2=2;
-		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// Créer une rencontre
+		 Rencontre r=new Rencontre(club1,club2,sc1,sc2);// CrÃ©er une rencontre
 		 r.Valider();
-		 assertEquals("Score Négatif",r.getErrorMessage());
+		 assertEquals("Score NÃ©gatif",r.getErrorMessage());
 	}
 
 
